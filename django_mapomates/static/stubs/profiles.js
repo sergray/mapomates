6e7b30320fb945750fe04dd7995ea0e9683cad01
Oleksiy Solyanyk <solex@odesk.com>
2011-07-16 18:15:30 +0300

    Simple map prototype (with stubs) implemented

---
+++ django_mapomates/static/stubs/profiles.js
@@ -0,0 +1,16 @@
+[
+    {
+        "name": "Oleksiy Solyanyk",
+        "odesk_profile": "https://www.odesk.com/users/~~bd69d92e638d03ff",
+        "pic": "https://odesk-prod-portraits.s3.amazonaws.com/Users:solex:PortraitUrl?AWSAccessKeyId=1XVAX3FNQZAFC9GJCFR2&Expires=2147483647&Signature=fBGWLGJW2iIcuUkWcSJS4ZcE4n4%3D",
+        "location": "Odesa, Ukraine",
+        "coords": [46.4914652120325, 30.7470634648506]
+    },
+    {
+        "name": "Sergey Panfilov",
+        "odesk_profile": "https://www.odesk.com/users/~~27b245de082bf9e3",
+        "pic": "https://odesk-prod-portraits.s3.amazonaws.com/Users:sergray:PortraitUrl?AWSAccessKeyId=1XVAX3FNQZAFC9GJCFR2&Expires=2147483647&Signature=gGqkxBncnilW8ZrS1Aqm6TOs3NA%3D",
+        "location": "Limassol, Cyprus",
+        "coords": [34.6735636, 33.0423629]
+    }
+]   
